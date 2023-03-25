d3da904dc0f179b5e1be514696b039af069e1011
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-25 12:32:54 +0100

    positive , negative

--- src/Ifelse.java
+++ src/Ifelse.java
@@ -1,7 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+//        Напишите программу,
+//        в которой пользователь вводит с клавиатуры целое число, а программа ему рассказывает,
+//        отрицательное это число, положительное или ноль, и делится ли оно на три.
 public class Ifelse {
-    public static void main(String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if (a > 0) {
+            System.out.println("number is positive ..");
+        }else {
+            System.out.println("number is negative..");
+        }
 
     }
 }
