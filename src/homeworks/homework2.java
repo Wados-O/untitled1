a50047950d22003ded182f2d734892dab2043cec
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-25 16:19:48 +0100

    add try,catch

--- src/homeworks/homework2.java
+++ src/homeworks/homework2.java
@@ -11,31 +11,51 @@ import java.util.Optional;
 public class homework2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Hi! How are you ?");
-        String hallo = br.readLine();
-        if (hallo.equalsIgnoreCase("fine and you?")) {
-            System.out.println("Oh you are so friendly.. ");
-        } else {
-            System.out.println("okay...");
-        }
-        System.out.println("What's your name? ");
-        String name = br.readLine();
-        if (name.equalsIgnoreCase("andrey")) {
-            System.out.println("This is my favorite");
-        }
-        System.out.println("Where are you from?");
-        String city = br.readLine();
-        if (city.equalsIgnoreCase("Berlin")) {
-            System.out.println("Cool !!! I am also!!!");
+        try {
+            System.out.println("Hi! How are you ?");
 
-            System.out.println("Maybe we can meet tomorrow in Berlin and Drink some coffee?");
-            String ask = br.readLine();
-            if (ask.equalsIgnoreCase("yes")) {
-                System.out.println("Send me message with answer in slak!!");
+            String hallo = br.readLine();
+            if (hallo.isEmpty()) {
+                System.out.println("String can't be empty: ");
+                hallo = br.readLine();
             }
-        } else {
-            System.out.println("Oh cool i was there !!!");
-            System.out.println("Sorry i gotta go!!! ");
+            if (hallo.equalsIgnoreCase("fine and you?")) {
+
+                System.out.println("Oh you are so friendly.. ");
+            } else {
+                System.out.println("okay...");
+            }
+
+            System.out.println("What's your name? ");
+            String name = br.readLine();
+
+            if (hallo.isEmpty()) {
+                System.out.println("String can't be empty: ");
+            }
+
+            if (name.equalsIgnoreCase("andrey")) {
+                System.out.println("This is my favorite");
+            }
+
+            System.out.println("Where are you from?");
+            String city = br.readLine();
+
+            if (city.equalsIgnoreCase("Berlin")) {
+                System.out.println("Cool !!! I am also!!!");
+
+                System.out.println("Maybe we can meet tomorrow in Berlin and Drink some coffee?");
+                String ask = br.readLine();
+                if (ask.equalsIgnoreCase("yes")) {
+                    System.out.println("Send me message with answer in slak!!");
+                }
+            } else {
+                System.out.println("Oh cool i was there !!!");
+                System.out.println("Sorry i gotta go!!! ");
+
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
         }
+
     }
 }
